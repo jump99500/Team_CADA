a64fdcf7a1491cbdafeb39f18af66475db6ee6bf
jump99500 <94940437+jump99500@users.noreply.github.com>
2021-12-21 12:20:07 +0900

    Delete 01_module directory

--- 01_module/00_key.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "id_rsa" {
-  key_name = var.key.name
-  public_key = file("./id_rsa.pub")
-}
--- 01_module/was.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo su -
-sudo yum -y update && sudo yum install -y python
-#sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i "s/#Port 22/Port 22/g" /etc/ssh/sshd_config
-sudo systemctl restart sshd
